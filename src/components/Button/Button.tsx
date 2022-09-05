865af63670975e67c74ad34b29e837109ae37198
JhonatanHzB <jhonatanhzb@outlook.com>
2022-09-04 23:15:19 -0500

    project setup

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Button.scss'
+
+type ButtonProps = {
+  handleClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+  variant: 'primary' | 'secondary' | 'dark'
+} & React.ComponentProps<'button'>
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  variant,
+  handleClick = () => {},
+  ...rest
+}) => {
+  return (
+    <button
+      className={`button button__${variant}`}
+      onClick={(event) => handleClick(event)}
+      {...rest}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
